fe588b08d9093e5b764d41935ca8c63843b41029
Moonyll <s.pouwel@laposte.net>
2019-01-17 15:49:00 +0100

    Jqqq3

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    var nombre = 0;
+    var aleatoire = Math.round(Math.random() * 100); // Nombre aléatoire entre 0 & 100
+    $('#sub').click(function () {
+        var choix = $('#nombre').val();
+        var reg = /^\d*$/;
+        if (reg.test(choix) && (choix<=100))
+        {
+            if (choix < aleatoire) {
+                $('#text').html('Le nombre est plus grand, essayez encore...' + ' (' + aleatoire + ')');
+                nombre++;
+            }
+            else if (choix > aleatoire) {
+                $('#text').html('Le nombre est plus petit, essayez encore...' + ' (' + aleatoire + ')');
+                nombre++;
+            }
+            else {
+                $('#text').html('Bravo vous avez trouvé le nombre en : ' + (nombre + 1) + ' essais. ' + ' (' + aleatoire + ')');
+            }
+        }
+        else
+        {
+        $('#text').html('Votre saisie est incorrecte, veuillez saisir un nombre correct.');
+        }
+    });
+});
